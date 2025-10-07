cb9a93338e1c7514d5f92c264860c163de899b35
wyq0918dev <wyq0918dev@gmail.com>
2025-10-07 17:37:06 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
 import 'app.dart';
-import 'capsule_placeholder.dart';
-import 'platform_image.dart';
 
 void main() => runApp(const TrebleKitApp());
-
-
-
-
