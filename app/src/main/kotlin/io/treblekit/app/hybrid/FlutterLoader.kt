c03afb25f63366e1e1cfa9e3ef711f93b39fb2e5
wyq0918dev <wyq0918dev@gmail.com>
2025-10-08 23:20:08 +0800

    commit

--- app/src/main/kotlin/io/treblekit/app/hybrid/FlutterLoader.kt
+++ app/src/main/kotlin/io/treblekit/app/hybrid/FlutterLoader.kt
@@ -1,14 +1,11 @@
 package io.treblekit.app.hybrid
 
 import android.app.Application
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.widget.ViewPager2
 import io.flutter.embedding.android.FlutterFragment
 import io.flutter.embedding.android.RenderMode
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
-import io.treblekit.app.common.FlutterHost
 
 /** Flutter引擎ID */
 private const val ENGINE_ID: String = "treblekit_flutter"
@@ -35,13 +32,3 @@ fun loadFlutterFragment(): FlutterFragment {
         RenderMode.texture,
     ).build()
 }
-
-///**
-// * 加载Flutter视图
-// */
-//fun FragmentActivity.loadFlutterView(): ViewPager2 {
-//    return ViewPager2(this@loadFlutterView).apply {
-//        isUserInputEnabled = false
-//        adapter = FlutterAdapter(activity = this@loadFlutterView)
-//    }
-//}
