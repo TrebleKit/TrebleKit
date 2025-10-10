2b36b088b321ae6a0557ec0d9bb160f7db8ba9ae
wyq0918dev <wyq0918dev@gmail.com>
2025-10-10 13:41:19 +0800

    commit

--- app/src/main/kotlin/io/treblekit/app/ui/theme/Color.kt
+++ app/src/main/kotlin/io/treblekit/app/ui/theme/Color.kt
@@ -2,42 +2,6 @@ package io.treblekit.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryDark: Color = Color(color = 0xFFA0CAFD)
-val onPrimaryDark: Color = Color(color = 0xFF003258)
-val primaryContainerDark: Color = Color(color = 0xFF194975)
-val onPrimaryContainerDark: Color = Color(color = 0xFFD1E4FF)
-val secondaryDark: Color = Color(color = 0xFFBBC7DB)
-val onSecondaryDark: Color = Color(color = 0xFF253140)
-val secondaryContainerDark: Color = Color(color = 0xFF3B4858)
-val onSecondaryContainerDark: Color = Color(color = 0xFFD7E3F7)
-val tertiaryDark: Color = Color(color = 0xFFD6BEE4)
-val onTertiaryDark: Color = Color(color = 0xFF3B2948)
-val tertiaryContainerDark: Color = Color(color = 0xFF523F5F)
-val onTertiaryContainerDark: Color = Color(color = 0xFFF2DAFF)
-val errorDark: Color = Color(color = 0xFFFFB4AB)
-val onErrorDark: Color = Color(color = 0xFF690005)
-val errorContainerDark: Color = Color(color = 0xFF93000A)
-val onErrorContainerDark: Color = Color(color = 0xFFFFDAD6)
-val backgroundDark: Color = Color(color = 0xFF111418)
-val onBackgroundDark: Color = Color(color = 0xFFE1E2E8)
-val surfaceDark: Color = Color(color = 0xFF111418)
-val onSurfaceDark: Color = Color(color = 0xFFE1E2E8)
-val surfaceVariantDark: Color = Color(color = 0xFF43474E)
-val onSurfaceVariantDark: Color = Color(color = 0xFFC3C7CF)
-val outlineDark: Color = Color(color = 0xFF8D9199)
-val outlineVariantDark: Color = Color(color = 0xFF43474E)
-val scrimDark: Color = Color(color = 0xFF000000)
-val inverseSurfaceDark: Color = Color(color = 0xFFE1E2E8)
-val inverseOnSurfaceDark: Color = Color(color = 0xFF2E3135)
-val inversePrimaryDark: Color = Color(color = 0xFF36618E)
-val surfaceDimDark: Color = Color(color = 0xFF111418)
-val surfaceBrightDark: Color = Color(color = 0xFF36393E)
-val surfaceContainerLowestDark: Color = Color(color = 0xFF0B0E13)
-val surfaceContainerLowDark: Color = Color(color = 0xFF191C20)
-val surfaceContainerDark: Color = Color(color = 0xFF1D2024)
-val surfaceContainerHighDark: Color = Color(color = 0xFF272A2F)
-val surfaceContainerHighestDark: Color = Color(color = 0xFF32353A)
-
 val bannerContainer = Color(color = 0xA0B71C1C) // 角标背景色
 val bannerContent = Color(color = 0xFFFFFFFF) // 角标文字色
 
