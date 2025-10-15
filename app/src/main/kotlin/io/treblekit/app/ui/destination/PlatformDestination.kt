77810c918ab775e7c86f5a90270f38209ecd04d5
wyq0918dev <wyq0918dev@gmail.com>
2025-10-15 22:52:36 +0800

    commit

--- app/src/main/kotlin/io/treblekit/app/ui/destination/PlatformDestination.kt
+++ app/src/main/kotlin/io/treblekit/app/ui/destination/PlatformDestination.kt
@@ -13,7 +13,6 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.twotone.Category
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,6 +20,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
@@ -32,7 +32,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,10 +54,7 @@ fun PlatformDestination(
     Column(
         modifier = modifier.fillMaxSize(),
     ) {
-        TopAppBar(
-            title = {
-                Text(text = "Treble平台")
-            },
+        Surface(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(
@@ -67,87 +63,94 @@ fun PlatformDestination(
                     end = 16.dp,
                     bottom = 8.dp,
                 )
-                .height(height = 56.dp)
-                .clip(shape = ContinuousRoundedRectangle(size = 16.dp)),
-            navigationIcon = {
-                IconButton(
-                    onClick = animateToDashboard,
-                ) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = null
-                    )
-                }
-            },
-            actions = {
-                if (aboutExpanded) AlertDialog(
-                    onDismissRequest = {
-                        aboutExpanded = false
-                    },
-                    title = {
-                        Text(text = "关于Treble平台")
-                    },
-                    icon = {
+                .height(height = 56.dp),
+            shape = ContinuousRoundedRectangle(size = 16.dp),
+        ) {
+            TopAppBar(
+                title = {
+                    Text(text = "Treble平台")
+                },
+                modifier = Modifier.fillMaxWidth(),
+                navigationIcon = {
+                    IconButton(
+                        onClick = animateToDashboard,
+                    ) {
                         Icon(
-                            imageVector = Icons.TwoTone.Category,
-                            contentDescription = null,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null
                         )
-                    },
-                    confirmButton = {
-                        TextButton(
-                            onClick = {
-                                aboutExpanded = false
-                            },
-                        ) {
-                            Text(text = "确定")
-                        }
-                    },
-                    text = {
-                        Text(text = "Treble平台是基于Dart和Kotlin自研的核心软件平台. 由FreeFEOS, EcosedKit和EbKit三大部分组成. 分别是核心组件, 应用层组件和平台能力桥接组件.")
-                    },
-                )
-                DropdownMenu(
-                    expanded = dropdownExpanded,
-                    onDismissRequest = {
-                        dropdownExpanded = false
-                    },
-                ) {
-                    DropdownMenuItem(
-                        leadingIcon = {
+                    }
+                },
+                actions = {
+                    if (aboutExpanded) AlertDialog(
+                        onDismissRequest = {
+                            aboutExpanded = false
+                        },
+                        title = {
+                            Text(text = "关于Treble平台")
+                        },
+                        icon = {
                             Icon(
-                                imageVector = Icons.Outlined.Info,
+                                imageVector = Icons.TwoTone.Category,
                                 contentDescription = null,
                             )
                         },
-                        text = {
-                            Text(text = "关于")
+                        confirmButton = {
+                            TextButton(
+                                onClick = {
+                                    aboutExpanded = false
+                                },
+                            ) {
+                                Text(text = "确定")
+                            }
                         },
-                        onClick = {
-                            aboutExpanded = true
+                        text = {
+                            Text(text = "Treble平台是基于Dart和Kotlin自研的核心软件平台. 由FreeFEOS, EcosedKit和EbKit三大部分组成. 分别是核心组件, 应用层组件和平台能力桥接组件.")
+                        },
+                    )
+                    DropdownMenu(
+                        expanded = dropdownExpanded,
+                        onDismissRequest = {
                             dropdownExpanded = false
                         },
-                    )
-                }
-                IconButton(
-                    onClick = {
-                        dropdownExpanded = true
-                    },
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.MoreVert,
-                        contentDescription = null,
-                    )
-                }
-            },
-            windowInsets = WindowInsets(),
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            ),
-        )
-        Card(
+                    ) {
+                        DropdownMenuItem(
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Outlined.Info,
+                                    contentDescription = null,
+                                )
+                            },
+                            text = {
+                                Text(text = "关于")
+                            },
+                            onClick = {
+                                aboutExpanded = true
+                                dropdownExpanded = false
+                            },
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            dropdownExpanded = true
+                        },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.MoreVert,
+                            contentDescription = null,
+                        )
+                    }
+                },
+                windowInsets = WindowInsets(),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                ),
+            )
+        }
+        Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(
