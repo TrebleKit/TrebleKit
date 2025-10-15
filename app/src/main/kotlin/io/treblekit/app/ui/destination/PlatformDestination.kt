00c2d0856c90da456e5f69e83dc9670c3599e604
wyq0918dev <wyq0918dev@gmail.com>
2025-10-15 22:25:09 +0800

    commit

--- app/src/main/kotlin/io/treblekit/app/ui/destination/PlatformDestination.kt
+++ app/src/main/kotlin/io/treblekit/app/ui/destination/PlatformDestination.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.twotone.Category
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,7 +21,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
@@ -33,7 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,16 +50,16 @@ fun PlatformDestination(
     animateToDashboard: () -> Unit = NoOnClick,
 ) {
     val inspection: Boolean = LocalInspectionMode.current
-    val inspectionModeText: String = stringResource(
-        id = R.string.inspection_mode_text,
-    )
-    var dropdownExpanded by remember { mutableStateOf(value = false) }
-    var aboutExpanded by remember { mutableStateOf(value = false) }
+    var dropdownExpanded: Boolean by remember { mutableStateOf(value = false) }
+    var aboutExpanded: Boolean by remember { mutableStateOf(value = false) }
     Column(
         modifier = modifier.fillMaxSize(),
     ) {
-        Surface(
-            modifier = modifier
+        TopAppBar(
+            title = {
+                Text(text = "Treble平台")
+            },
+            modifier = Modifier
                 .fillMaxWidth()
                 .padding(
                     start = 16.dp,
@@ -68,90 +67,87 @@ fun PlatformDestination(
                     end = 16.dp,
                     bottom = 8.dp,
                 )
-                .height(height = 56.dp),
-            shape = ContinuousRoundedRectangle(size = 16.dp),
-            color = MaterialTheme.colorScheme.primaryContainer,
-        ) {
-            TopAppBar(
-                title = {
-                    Text(text = "Treble平台")
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                navigationIcon = {
-                    IconButton(
-                        onClick = animateToDashboard,
-                    ) {
+                .height(height = 56.dp)
+                .clip(shape = ContinuousRoundedRectangle(size = 16.dp)),
+            navigationIcon = {
+                IconButton(
+                    onClick = animateToDashboard,
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            },
+            actions = {
+                if (aboutExpanded) AlertDialog(
+                    onDismissRequest = {
+                        aboutExpanded = false
+                    },
+                    title = {
+                        Text(text = "关于Treble平台")
+                    },
+                    icon = {
                         Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = null
+                            imageVector = Icons.TwoTone.Category,
+                            contentDescription = null,
                         )
-                    }
-                },
-                actions = {
-                    if (aboutExpanded) AlertDialog(
-                        onDismissRequest = {
-                            aboutExpanded = false
-                        },
-                        title = {
-                            Text(text = "关于Treble平台")
-                        },
-                        icon = {
+                    },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                aboutExpanded = false
+                            },
+                        ) {
+                            Text(text = "确定")
+                        }
+                    },
+                    text = {
+                        Text(text = "Treble平台是基于Dart和Kotlin自研的核心软件平台. 由FreeFEOS, EcosedKit和EbKit三大部分组成. 分别是核心组件, 应用层组件和平台能力桥接组件.")
+                    },
+                )
+                DropdownMenu(
+                    expanded = dropdownExpanded,
+                    onDismissRequest = {
+                        dropdownExpanded = false
+                    },
+                ) {
+                    DropdownMenuItem(
+                        leadingIcon = {
                             Icon(
-                                imageVector = Icons.TwoTone.Category,
+                                imageVector = Icons.Outlined.Info,
                                 contentDescription = null,
                             )
                         },
-                        confirmButton = {
-                            TextButton(onClick = { aboutExpanded = false }) {
-                                Text(text = "确定")
-                            }
-                        },
                         text = {
-                            Text(text = "Treble平台是基于Dart和Kotlin自研的核心软件平台. 由FreeFEOS, EcosedKit和EbKit三大部分组成. 分别是核心组件, 应用层组件和平台能力桥接组件.")
+                            Text(text = "关于")
                         },
-                    )
-                    DropdownMenu(
-                        expanded = dropdownExpanded,
-                        onDismissRequest = {
+                        onClick = {
+                            aboutExpanded = true
                             dropdownExpanded = false
                         },
-                    ) {
-                        DropdownMenuItem(
-                            leadingIcon = {
-                                Icon(
-                                    imageVector = Icons.Outlined.Info,
-                                    contentDescription = null,
-                                )
-                            },
-                            text = {
-                                Text(text = "关于")
-                            },
-                            onClick = {
-                                aboutExpanded = true
-                                dropdownExpanded = false
-                            },
-                        )
-                    }
-                    IconButton(
-                        onClick = {
-                            dropdownExpanded = true
-                        },
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.MoreVert,
-                            contentDescription = null,
-                        )
-                    }
-                },
-                windowInsets = WindowInsets(),
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Transparent,
-                ),
-            )
-        }
-        Surface(
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        dropdownExpanded = true
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.MoreVert,
+                        contentDescription = null,
+                    )
+                }
+            },
+            windowInsets = WindowInsets(),
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            ),
+        )
+        Card(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(
@@ -182,7 +178,7 @@ fun PlatformDestination(
                             .padding(paddingValues = innerPadding),
                         contentAlignment = Alignment.Center,
                     ) {
-                        Text(text = inspectionModeText)
+                        Text(text = stringResource(id = R.string.inspection_mode_text))
                     }
                 }
             }
@@ -190,7 +186,7 @@ fun PlatformDestination(
     }
 }
 
-@Preview
+@Preview(showBackground = false)
 @Composable
 fun PlatformDestinationPreview() {
     TrebleKitTheme {
