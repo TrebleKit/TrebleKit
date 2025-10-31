ead1a516f52033f4aa49ff3b1ac21ce9c519911c
wyq0918dev <wyq0918dev@gmail.com>
2025-11-01 02:18:11 +0800

    commit

--- app/src/main/kotlin/io/treblekit/ui/components/AppBackground.kt
+++ app/src/main/kotlin/io/treblekit/ui/components/AppBackground.kt
@@ -1,16 +1,18 @@
 package io.treblekit.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.IntOffset
 import com.kyant.backdrop.backdrops.LayerBackdrop
 import com.kyant.backdrop.backdrops.layerBackdrop
 import com.kyant.backdrop.backdrops.rememberLayerBackdrop
@@ -29,29 +31,46 @@ fun AppBackground(
     content: @Composable BoxScope.(LayerBackdrop) -> Unit = {},
 ) {
     val backdrop: LayerBackdrop = rememberLayerBackdrop() // Backdrop
-    val density: Density = LocalDensity.current // Density
     val inspection: Boolean = LocalInspectionMode.current // 检查模式
-    BoxWithConstraints(
-        modifier = modifier
-            .fillMaxSize()
-            .background(color = appBackground) // 背景色
-            .layerBackdrop(backdrop = backdrop), // Backdrop
-    ) {
-        if (!inspection) {
-            // 非检查模式下, 显示流光背景效果
-            ViewFactory(
+    Box(modifier = modifier.fillMaxSize()) {
+//        BoxWithConstraints(
+//            modifier = modifier
+//                .fillMaxSize()
+//                .background(color = appBackground) // 背景色
+//                .layerBackdrop(backdrop = backdrop), // Backdrop
+//        ) {
+//            // 非检查模式下, 显示流光背景效果
+//            if (!inspection) ViewFactory(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .offset(y = maxHeight / 7 * 2), // 偏移到屏幕高度7分之2处
+//            ) {
+//                getEffectView // 流光背景效果
+//            }
+//        }
+
+        Box(
+            modifier = modifier
+                .layout { measurable, constraints ->
+                    val placeable = measurable.measure(constraints)
+                    layout(placeable.width, placeable.height) {
+                        placeable.placeRelative(0, 0 + constraints.maxHeight / 7 * 2)
+                    }
+                }
+                .background(color = appBackground) // 背景色
+                .layerBackdrop(backdrop = backdrop), // Backdrop
+        ) {
+            // 非检查模式下, 显示流光背景效果s
+            if (!inspection) ViewFactory(
                 modifier = Modifier
                     .fillMaxSize()
-                    .offset(
-                        y = with(receiver = density) {
-                            return@with (constraints.maxHeight / 7 * 2).toDp()
-                        } // 偏移到屏幕高度7分之2处
-                    ),
+//                    .offset(y = maxHeight / 7 * 2), // 偏移到屏幕高度7分之2处
             ) {
                 getEffectView // 流光背景效果
             }
         }
-        content.invoke(this@BoxWithConstraints, backdrop) // 前景内容
+        // 前景内容
+        content.invoke(this@Box, backdrop)
     }
 }
 
